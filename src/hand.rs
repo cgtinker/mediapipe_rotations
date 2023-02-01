e7141a73ab474cd219049fc5775d5afffb09751b
dhsu <=>
2023-02-01 02:49:57 +0100

    testbase

--- src/hand.rs
+++ src/hand.rs
@@ -12,13 +12,27 @@ const JOINTS: [[usize; 3]; 3] = [[0, 1, 2], [1, 2, 3], [2, 3, 4]];
 
 
 pub fn main(hand: &[[f32; 3]]) -> [Quaternion; 22] {
+    let mut data = to_vectors(hand);
+    set_hand_origin(&mut data);
+
+    let mut angles = [[f32::NAN; 2]; 22];
+    x_angles(&data, &mut angles);
+    z_angles(&data, &mut angles);
+
+    let mut rotation_data = [Quaternion::NAN; 22];
+    angles_to_quaternions(&angles, &mut rotation_data);
+    hand_rotation(&data, &mut rotation_data);
+
+    return rotation_data;
+}
+
+/// Converts data to Vector3s.
+fn to_vectors(hand: &[[f32; 3]]) -> [Vector3; 22] {
     let mut data: [Vector3; 22] = [Vector3::ZERO; 22];
     for i in 0..21 {
         data[i] = Vector3::from_array(hand[i]);
     }
-    set_hand_origin(&mut data);
-    let rotation_data = calculate_rotations(&data);
-    return rotation_data;
+    return data;
 }
 
 /// Sets hand origin to wrist.
@@ -26,8 +40,6 @@ fn set_hand_origin(data: &mut [Vector3; 22]) {
     let offset = data[0];
     for i in 0..22 {
         data[i] -= offset;
-    }
-    for i in 0..22 {
         let tmp = data[i];
         data[i].x = -tmp.x;
         data[i].y = tmp.z;
@@ -36,12 +48,8 @@ fn set_hand_origin(data: &mut [Vector3; 22]) {
     data[21] = offset;
 }
 
-fn calculate_rotations(data: &[Vector3; 22]) -> [Quaternion; 22]{
-    let mut angles = [[f32::NAN; 2]; 22];
-    let mut rotation_data = [Quaternion::NAN; 22];
-    x_angles(&data, &mut angles);
-    z_angles(&data, &mut angles);
-    hand_rotation(&data, &mut rotation_data);
+/// Attempts to convert finger angles to quaternions.
+fn angles_to_quaternions(angles: &[[f32;2]; 22], rotation_data: &mut [Quaternion; 22]) {
     for (i, xz) in angles.iter().enumerate() {
         if !xz[0].is_nan() {
             rotation_data[i] = Quaternion::from_rotation_x(xz[0]);
@@ -50,7 +58,6 @@ fn calculate_rotations(data: &[Vector3; 22]) -> [Quaternion; 22]{
             rotation_data[i] *= Quaternion::from_rotation_z(xz[1]);
         }
     }
-    return rotation_data;
 }
 
 /// Projects finger points on plane, then calculates finger x-angles.
@@ -152,3 +159,34 @@ fn hand_rotation(data: &[Vector3; 22], rotation_data: &mut [Quaternion; 22]) {
     let tangent = (binormal.cross(normal)).normalize();
     rotation_data[0] = Quaternion::from_rotation_axes(tangent, normal, binormal);
 }
+
+#[cfg(test)]
+mod test {
+    use cgt_math::{Quaternion, Vector3};
+    #[test]
+    fn impl_test() {
+        let hand_data = [[-0.012344579212367535, 0.07004635035991669, 0.020521901547908783], [0.018071463331580162, 0.047368425875902176, 0.010523390956223011], [0.03255487233400345, 0.016385573893785477, -0.0011732536368072033], [0.037621572613716125, -0.017625989392399788, -0.013580389320850372], [0.043106138706207275, -0.05177343264222145, -0.017558827996253967], [0.024736206978559494, -0.006148995831608772, 0.0019370221998542547], [0.026668652892112732, -0.03547884523868561, -0.006494760047644377], [0.02291758358478546, -0.05428066849708557, -0.011178224347531796], [0.025358645245432854, -0.07094687223434448, -0.03112722560763359], [0.0005194954574108124, -0.0025673473719507456, 0.005111261270940304], [0.003210199996829033, -0.03972770646214485, -0.004665873944759369], [-0.002873774617910385, -0.06354730576276779, -0.018868273124098778], [0.004980511963367462, -0.08323581516742706, -0.030609922483563423], [-0.01502157561480999, 0.0035259551368653774, -0.0005771743599325418], [-0.018982525914907455, -0.03117505833506584, -0.007334231864660978], [-0.01772765815258026, -0.05211472511291504, -0.014353149570524693], [-0.013936810195446014, -0.07475992292165756, -0.028724966570734978], [-0.03519390895962715, 0.01385025680065155, -0.0037085190415382385], [-0.03892548382282257, -0.008683949708938599, -0.00501153664663434], [-0.03714082017540932, -0.02858530357480049, -0.010903152637183666], [-0.03820459172129631, -0.048742808401584625, -0.025562860071659088]];
+        let mut data: [Vector3; 22] = super::to_vectors(&hand_data);
+
+        // check if reseting pose works
+        super::set_hand_origin(&mut data);
+        assert_eq!(data[0], Vector3::ZERO);
+        assert_ne!(data[5], Vector3::ZERO);
+
+        let mut angles = [[f32::NAN; 2]; 22];
+        super::x_angles(&data, &mut angles);
+        super::z_angles(&data, &mut angles);
+
+        // check if rotations have been calculated
+        let mut rotation_data = [Quaternion::NAN; 22];
+        super::angles_to_quaternions(&angles, &mut rotation_data);
+        super::hand_rotation(&data, &mut rotation_data);
+
+        for finger in super::FINGERS {
+            for i in 0..3 {
+                assert!(rotation_data[finger[i]].is_finite());
+            }
+        }
+    }
+}
+
